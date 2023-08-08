18ae0d8fd56945a700c71729abe5782007836432
parkhomchukalexei <parkhomchuk.alexei@gmail.com>
2023-08-08 11:47:31 +0300

    added new CORE table and table_data controller! upd:added migration

--- heaven_adminpanel/settings.py
+++ heaven_adminpanel/settings.py
@@ -158,15 +158,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     # ...
     "127.0.0.1",
-    "heavenfront.herokuapp.com"
+    "https://heavenfront.herokuapp.com/"
     # ...
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:4200",
-    'https://heavenfront.herokuapp.com',
-    'https://heavensite.herokuapp.com'
+    "https://heavenfront.herokuapp.com/",
+    "https://heavensite.herokuapp.com/"
 ]
 
 REST_FRAMEWORK = {
