8e33cfac2bba41192e2c922c62177eb3b88815a9
parkhomchukalexei <parkhomchuk.alexei@gmail.com>
2023-08-08 11:26:46 +0300

    added new CORE table and table_data controller! upd:added migration

--- users/models.py
+++ users/models.py
@@ -37,18 +37,18 @@ class Client(models.Model):
     country = models.CharField(max_length=255, null=True)
     photo = models.CharField(max_length=255, null=True)
     date_of_birth = models.DateField(null=True)
-    project_info = models.JSONField(default={
-            'AnastasiaDating': [],
-            'DateMe': [],
-            'CharmDate': [],
-            'GoldenBride': [],
-            'Fansly': [],
-            'OnlyFans': [],
-            'Jump4Love': []})
-    socials = models.JSONField(default= {'Instagram': [],
-            'Facebook': [],
-            'Reddit': [],
-            })
+    # project_info = models.JSONField(default={
+    #         'AnastasiaDating': [],
+    #         'DateMe': [],
+    #         'CharmDate': [],
+    #         'GoldenBride': [],
+    #         'Fansly': [],
+    #         'OnlyFans': [],
+    #         'Jump4Love': []})
+    # socials = models.JSONField(default= {'Instagram': [],
+    #         'Facebook': [],
+    #         'Reddit': [],
+    #         })
     managers = models.ManyToManyField(User)
 
     def __str__(self):
